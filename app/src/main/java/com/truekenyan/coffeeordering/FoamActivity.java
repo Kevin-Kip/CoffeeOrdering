c706e3af36ada3e649b7ed5ff6c581bba8177985
Kevin-Kip <masterfork5@gmail.com>
2018-06-06 10:53:42 +0300

    Fixed a few things

--- app/src/main/java/com/truekenyan/coffeeordering/FoamActivity.java
+++ app/src/main/java/com/truekenyan/coffeeordering/FoamActivity.java
@@ -2,7 +2,6 @@ package com.truekenyan.coffeeordering;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
